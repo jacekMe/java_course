187afccb772ad19b701e44e41b5e6501253fa6c8
jacekMe <jacekmeres@gmail.com>
2025-01-30 21:48:56 +0100

    creating loop for

---
+++ podstawy_javy/LoopFor.java
@@ -0,0 +1,32 @@
+package podstawy_javy;
+
+public class LoopFor {
+    public static void main(String[] args) {
+        String[] cyclingRaces =
+                {
+                        "Strade Bianche",
+                        "Milan-Sanremo",
+                        "Ronde van Vlaanderen",
+                        "Paris-Roubaix",
+                        "La Fleche Wallonge",
+                        "Liege-Bastogne-Liege",
+                        "Giro d'Italia",
+                        "Tour de France",
+                        "La Vuelta Espana",
+                        "Il Lombardia"
+                };
+
+        // for(INICJALIZACJA ZMIENNYCH; WARUNEK PĘTLI; CO MA ZROBIĆ PO WYKONANIU WSZYSTKICH INSTR. W PĘTLI)
+//        for(int i = 0; i < cyclingRaces.length; i++)
+//        {
+//            System.out.println(cyclingRaces[i]);
+//        }
+
+        // ENHANCED FOR
+
+        for(String nameRace: cyclingRaces)
+        {
+            System.out.println(nameRace);
+        }
+    }
+}
