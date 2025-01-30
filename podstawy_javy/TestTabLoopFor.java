f6a1a0141f784f9c06a2114d2d1c9f55ea10b6b2
jacekMe <jacekmeres@gmail.com>
2025-01-30 21:49:25 +0100

    exercise loop for

---
+++ podstawy_javy/TestTabLoopFor.java
@@ -0,0 +1,31 @@
+package podstawy_javy;
+
+public class TestTabLoopFor {
+    public static void main(String[] args) {
+        String[] cyclingRaces =
+                {
+                        "Strade Bianche",
+                        "Milan-Sanremo",
+                        "Ronde van Vlaanderen",
+                        "Paris-Roubaix",
+                        "La Fleche Wallonge",
+                        "Liege-Bastogne-Liege",
+                        "Giro d'Italia",
+                        "Tour de France",
+                        "La Vuelta Espana",
+                        "Il Lombardia"
+                };
+
+//        for(int i = 0; i < cyclingRaces.length; i++)
+//        {
+//            System.out.println(cyclingRaces[i]);
+//        }
+
+        // ENHANCED FOR
+
+        for(String nameRaces: cyclingRaces)
+        {
+            System.out.println(nameRaces);
+        }
+    }
+}
